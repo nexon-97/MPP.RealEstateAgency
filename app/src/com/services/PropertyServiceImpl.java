867636d182f68a52b7bb5fcf0e5d817b44baa374
MakersMS <aurisgurik@mail.ru>
2017-04-28 13:38:21 +0300

    Added block field in property (Modified controllers)

--- app/src/com/services/PropertyServiceImpl.java
+++ app/src/com/services/PropertyServiceImpl.java
@@ -37,21 +37,22 @@ public class PropertyServiceImpl extends BaseService implements PropertyService 
         boolean isCorrectFields = true;
 
         property.setType( PropertyType.valueOf(params.get("type")[0]));
-        property.setCity(params.get("city")[0]);
-        property.setStreet(params.get("street")[0]);
-        property.setHouseNumber(Integer.parseInt(params.get("houseNumber")[0]));
-        property.setFlatNumber(Integer.parseInt(params.get("flatNumber")[0]));
-        property.setRoomsCount(Integer.parseInt(params.get("flatNumber")[0]));
-        property.setArea(Integer.parseInt(params.get("area")[0]));
-        property.setDistanceToSubway(Integer.parseInt(params.get("subway")[0]));
-        property.setDistanceToTransportStop(Integer.parseInt(params.get("bus")[0]));
+        property.setCity(getStringParam(params, "city"));
+        property.setStreet(getStringParam(params, "street"));
+        property.setHouseNumber(getNotNullIntParam(params, "houseNumber"));
+        property.setBlockNumber(getIntParam(params, "blockNumber"));
+        property.setFlatNumber(getIntParam(params, "flatNumber"));
+        property.setRoomsCount(getIntParam(params, "roomsCount"));
+        property.setArea(getNotNullIntParam(params, "area"));
+        property.setDistanceToSubway(getIntParam(params, "subway"));
+        property.setDistanceToTransportStop(getIntParam(params, "bus"));
         property.setHasFurniture(isChecked(params.get("furniture")));
         property.setHasInternet(isChecked(params.get("internet")));
         property.setHasTv(isChecked(params.get("tv")));
         property.setHasPhone(isChecked(params.get("phone")));
         property.setHasFridge(isChecked(params.get("fridge")));
         property.setHasStove(isChecked(params.get("stove")));
-        property.setDescription(params.get("description")[0]);
+        property.setDescription(getDescription(params, "description"));
 
         User loggedUser = ServiceManager.getInstance().getAuthService().getLoggedUser();
         property.setOwner(loggedUser);
@@ -66,8 +67,13 @@ public class PropertyServiceImpl extends BaseService implements PropertyService 
             isCorrectFields = false;
         }
 
-        if (!checkNumber(property.getHouseNumber())){
+        if (!checkNotNullNumber(property.getHouseNumber())){
             property.setHouseNumber(-1);
+            isCorrectFields = false;
+        }
+
+        if (!checkNumber(property.getBlockNumber())){
+            property.setBlockNumber(-1);
             isCorrectFields = false;
         }
 
@@ -81,7 +87,7 @@ public class PropertyServiceImpl extends BaseService implements PropertyService 
             isCorrectFields = false;
         }
 
-        if (!checkNumber(property.getArea())){
+        if (!checkNotNullNumber(property.getArea())){
             property.setArea(-1);
             isCorrectFields = false;
         }
@@ -127,21 +133,52 @@ public class PropertyServiceImpl extends BaseService implements PropertyService 
         return propertyDAO.getPropertiesOwnedByUser(user);
     }
 
+    private String getDescription(Map<String, String[]> params, String paramKey){
+        String[] paramValues = params.get(paramKey);
+        if (paramValues == null || paramValues[0]== null) return "";
+        else return paramValues[0];
+    }
+
+    private String getStringParam(Map<String, String[]> params, String paramKey){
+        String[] paramValues = params.get(paramKey);
+        if (paramValues == null || paramValues[0]== null || paramValues[0] == "") return null;
+        else return paramValues[0].trim();
+    }
+
+    private int getNotNullIntParam(Map<String, String[]> params, String paramKey){
+        String[] paramValues = params.get(paramKey);
+        if (paramValues == null || paramValues[0]==null || paramValues[0] == "") return -1;
+        else return Integer.parseInt(paramValues[0]);
+    }
+
+    private Integer getIntParam(Map<String, String[]> params, String paramKey){
+        String[] paramValues = params.get(paramKey);
+        if (paramValues == null || paramValues[0]==null || paramValues[0] == "") return null;
+        else return Integer.parseInt(paramValues[0]);
+    }
+
     private boolean isChecked(String[] value){
         return value!=null;
     }
 
     private boolean checkStringName(String data){
+        if (data == null) return false;
         Pattern pattern = Pattern.compile("^[a-zA-Zа-яёА-ЯЁ][a-zA-Zа-яёА-ЯЁ\\-'\\s]*$");
         Matcher matcher = pattern.matcher(data);
         return matcher.find();
     }
 
     private boolean checkDistance(Integer data){
-        return data>=0;
+        if (data==null) return true;
+        else return data>=0;
     }
 
     private boolean checkNumber(Integer data){
+        if (data==null) return true;
+        else return checkNotNullNumber(data);
+    }
+
+    private boolean checkNotNullNumber(Integer data) {
         return data>0;
     }
 }

22718de38deaff7ddcc2f2afff4c747286179090
Denis Ponyakov <nexons.97@gmail.com>
2017-05-01 23:18:06 +0300

    Fixed codestyle

--- app/src/com/controller/PropertyController.java
+++ app/src/com/controller/PropertyController.java
@@ -4,10 +4,6 @@ import com.model.Property;
 import com.model.PropertyType;
 import com.services.PropertyService;
 import com.services.shared.ServiceManager;
-import com.utils.request.BooleanParameter;
-import com.utils.request.FilterParameter;
-import com.utils.request.IntegerRangeParameter;
-import com.utils.request.PropertyFilterParamId;
 import com.utils.request.validator.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,9 +11,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletResponse;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -44,9 +37,7 @@ public class PropertyController extends BaseController  {
         if (property == null) {
             model.put("msg", "Can't find requested property!");
             return buildModelAndView("../error_message");
-        }
-        else
-        {
+        } else {
             model.put("property", property);
         }
 
@@ -82,39 +73,37 @@ public class PropertyController extends BaseController  {
                 boolean isAdded = propertyService.addProperty(requestValidator);
                 if (isAdded) {
                     return redirect("/");
-                }
-                else {
+                } else {
                     return getViewWithErrors("addError", "Ошибка при добавлении собственности", requestValidator.getValidatedValues());
                 }
-            }
-            else {
+            } else {
                 return getViewWithErrors("errors", requestValidator.getErrorMessageMap(), requestValidator.getValidatedValues());
             }
         }
     }
 
-    private RequestValidationChain buildPropertyValidationChain(){
+    private RequestValidationChain buildPropertyValidationChain() {
         return new RequestValidationChain()
-                .addValidator(new EnumParameterValidator<>(PropertyType.class, "type"))
-                .addValidator(new PropertyStringParameterValidator("city",  false))
-                .addValidator(new PropertyStringParameterValidator("street", false))
-                .addValidator(new IntegerParameterValidator("houseNumber", false))
-                .addValidator(new IntegerParameterValidator("blockNumber", true))
-                .addValidator(new IntegerParameterValidator("flatNumber", true))
-                .addValidator(new IntegerParameterValidator("roomsCount", true))
-                .addValidator(new IntegerParameterValidator("area", false))
-                .addValidator(new IntegerParameterValidator("subway", true))
-                .addValidator(new IntegerParameterValidator("bus",true))
-                .addValidator(new BooleanParameterValidator("furniture"))
-                .addValidator(new BooleanParameterValidator("internet"))
-                .addValidator(new BooleanParameterValidator("tv"))
-                .addValidator(new BooleanParameterValidator("phone"))
-                .addValidator(new BooleanParameterValidator("fridge"))
-                .addValidator(new BooleanParameterValidator("stove"))
-                .addValidator(new PropertyStringParameterValidator("description", false));
+            .addValidator(new EnumParameterValidator<>(PropertyType.class, "type"))
+            .addValidator(new PropertyStringParameterValidator("city",  false))
+            .addValidator(new PropertyStringParameterValidator("street", false))
+            .addValidator(new IntegerParameterValidator("houseNumber", false))
+            .addValidator(new IntegerParameterValidator("blockNumber", true))
+            .addValidator(new IntegerParameterValidator("flatNumber", true))
+            .addValidator(new IntegerParameterValidator("roomsCount", true))
+            .addValidator(new IntegerParameterValidator("area", false))
+            .addValidator(new IntegerParameterValidator("subway", true))
+            .addValidator(new IntegerParameterValidator("bus",true))
+            .addValidator(new BooleanParameterValidator("furniture"))
+            .addValidator(new BooleanParameterValidator("internet"))
+            .addValidator(new BooleanParameterValidator("tv"))
+            .addValidator(new BooleanParameterValidator("phone"))
+            .addValidator(new BooleanParameterValidator("fridge"))
+            .addValidator(new BooleanParameterValidator("stove"))
+            .addValidator(new PropertyStringParameterValidator("description", false));
     }
 
-    private ModelAndView getViewWithErrors(String key, Object error, Object values){
+    private ModelAndView getViewWithErrors(String key, Object error, Object values) {
         Map<String, Object> model = ServiceManager.getInstance().getSharedResources().getModel();
         PropertyType[] types = PropertyType.values();
         model.put(key, error);
--- app/src/com/utils/request/validator/PropertyStringParameterValidator.java
+++ app/src/com/utils/request/validator/PropertyStringParameterValidator.java
@@ -1,6 +1,5 @@
 package com.utils.request.validator;
 
-
 import com.services.shared.ServiceManager;
 
 import javax.servlet.http.HttpServletRequest;
@@ -9,42 +8,42 @@ import java.util.regex.Pattern;
 
 public class PropertyStringParameterValidator extends StringParameterValidator {
 
-    public PropertyStringParameterValidator(String paramName, boolean isNullAllowed){
-
+    public PropertyStringParameterValidator(String paramName, boolean isNullAllowed) {
         super(paramName, isNullAllowed);
     }
 
     @Override
     public boolean validate() {
         this.errorMessage = null;
-        try{
+        try {
             HttpServletRequest request = ServiceManager.getInstance().getSharedResources().getRequest();
             String paramValue = request.getParameter(this.paramName).trim();
-            if(paramValue == ""){
+
+            if (paramValue.length() == 0) {
                 return checkNullPermission(String.format("Параметр '%s' отсутствует", paramName));
-            } else if (!checkRegularExpression(paramValue)){
+            } else if (!checkRegularExpression(paramValue)) {
                 this.errorMessage = String.format("Параметр '%s' может содержать русские буквы, цифры и символы '-' ''' ' '", paramName);
                 return false;
             } else {
                 this.value = paramValue;
                 return true;
             }
-        }
-        catch(NullPointerException e){
+        } catch(NullPointerException e) {
             return checkNullPermission(String.format("Параметр '%s' отсутствует", paramName));
         }
     }
 
-    private boolean checkNullPermission(String errorMessage){
+    private boolean checkNullPermission(String errorMessage) {
         if (isNullAllowed) {
             this.value = null;
             return true;
         }
+
         this.errorMessage = errorMessage;
         return false;
     }
 
-    private boolean checkRegularExpression(String value){
+    private boolean checkRegularExpression(String value) {
         Pattern pattern = Pattern.compile("^[а-яёА-ЯЁ][а-яёА-ЯЁ\\-'\\s]*$");
         Matcher matcher = pattern.matcher(value);
         return matcher.find();
